449bff3cc68cd63bc5c92900f23924c6841931f2
Projektgruppen User <s6parauh@uni-bonn.de>
2023-08-21 15:51:47 +0200

    Anzeige der Bounding Boxes auf Video

--- src/detection/test.py
+++ src/detection/test.py
@@ -3,16 +3,10 @@ import cv2
 import os
 import math 
 
+model = YOLO("yolo-Weights/yolov8n.pt")
 
 source = "/home/projekt/Dokumente/PG/BeispielVideoWebcam.mp4"
 cap = cv2.VideoCapture(source)
-# cap.set(3, 640)
-# cap.set(4, 480)
-
-# model
-model = YOLO("yolo-Weights/yolov8n.pt")
-
-
 
 # object classes
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
@@ -30,53 +24,40 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
 
 while True:
     success, img = cap.read()
-    results = model(source, stream=True)
-
-    print(35)
+    results = model(img, stream=True)
 
     # coordinates
     for r in results:
         boxes = r.boxes
 
         for box in boxes:
+            # bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+
+            # put box in cam
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+            # confidence
+            confidence = math.ceil((box.conf[0]*100))/100
+            print("Confidence --->",confidence)
+
+            # class name
             cls = int(box.cls[0])
-            if(classNames[cls]== "person"):
-                # bounding box
-                x1, y1, x2, y2 = box.xyxy[0]
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+            print("Class name -->", classNames[cls])
 
-                # put box in cam
-                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            # object details
+            org = [x1, y1]
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale = 1
+            color = (255, 0, 0)
+            thickness = 2
 
-                # confidence
-                confidence = math.ceil((box.conf[0]*100))/100
-                print("Confidence --->",confidence)
-
-                # class name
-               
-                print("Class name -->", classNames[cls])
-
-                # object details
-                org = [x1, y1]
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                fontScale = 1
-                color = (255, 0, 0)
-                thickness = 2
-
-            
-                
-
-                cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
-
-        # cv2.imshow('Webcam', img)
-        # if cv2.waitKey(1) == ord('q'):
-        #     break
-
-        path = '/home/projekt/Dokumente'
-        cv2.imwrite(os.path.join(path , 'Test.jpg'), img)
-        exit()
-
-    exit()
+            cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+    print("ok")
+    cv2.imshow('Webcam', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
