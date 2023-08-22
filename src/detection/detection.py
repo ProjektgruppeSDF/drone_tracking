f5a9dde7ba818d0dc46bdde8a96b02aca8277a54
Projektgruppen User <s6parauh@uni-bonn.de>
2023-08-22 10:16:26 +0200

    Projektstruktur und gitignore

--- src/detection/detection.py
+++ src/detection/detection.py
@@ -3,63 +3,54 @@ import cv2
 import os
 import math 
 
-model = YOLO("resources/yolo-Weights/yolov8n.pt")
-
-source = "resources/BeispielVideoWebcam.mp4"
-cap = cv2.VideoCapture(source)
-
-# object classes
-classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear", "hair drier", "toothbrush"
-              ]
+from .detection_config import model_weights, source, classNames
 
 
-while True:
-    success, img = cap.read()
-    results = model(img, stream=True)
 
-    # coordinates
-    for r in results:
-        boxes = r.boxes
 
-        for box in boxes:
 
-            # class name
-            cls = int(box.cls[0])
-            print("Class name -->", classNames[cls])
+# TODO: vernünftiges Refactoring
+def detect_person_on_videostream():
+    model = YOLO(model_weights)
+    cap = cv2.VideoCapture(source)
+    while True:
+        success, img = cap.read()
+        results = model(img, stream=True)
 
-            if(classNames[cls] == "person"):
-                # bounding box
-                x1, y1, x2, y2 = box.xyxy[0]
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+        # coordinates
+        for r in results:
+            boxes = r.boxes
 
-                # put box in cam
-                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            for box in boxes:
 
-                # confidence
-                confidence = math.ceil((box.conf[0]*100))/100
-                print("Confidence --->",confidence)
-                
-                # object details
-                org = [x1, y1]
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                fontScale = 1
-                color = (255, 0, 0)
-                thickness = 2
+                # class name
+                cls = int(box.cls[0])
+                print("Class name -->", classNames[cls])
 
-                cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
-    print("ok")
-    cv2.imshow('Webcam', img)
-    if cv2.waitKey(1) == ord('q'):
-        break
+                if(classNames[cls] == "person"):
+                    # bounding box
+                    x1, y1, x2, y2 = box.xyxy[0]
+                    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
 
-cap.release()
-cv2.destroyAllWindows()
+                    # put box in cam
+                    cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+                    # confidence
+                    confidence = math.ceil((box.conf[0]*100))/100
+                    print("Confidence --->",confidence)
+                    
+                    # object details
+                    org = [x1, y1]
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    fontScale = 1
+                    color = (255, 0, 0)
+                    thickness = 2
+
+                    cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+        print("ok")
+        cv2.imshow('Webcam', img)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
---
+++ src/main.py
@@ -0,0 +1,5 @@
+from detection.detection import detect_person_on_videostream
+
+
+if __name__ == "__main__":
+    detect_person_on_videostream()
