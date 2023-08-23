1894d4b19f4b078b17cf64160c5d078a844d560c
Projektgruppen User <s6parauh@uni-bonn.de>
2023-08-23 10:35:30 +0200

    Kamerasteuerung getestet

--- src/tracking/tracker.py
+++ src/tracking/tracker.py
@@ -1,6 +1,28 @@
 
+import requests
+
+
+from detection.detection_result import DetectionBox
+
+
 
 class Tracker:
     
     def track(self, detection_results):
-        pass
+        #xCenter = 
+        #boundingCenter = tracking_point(detection_results.confidence)
+        try: 
+            detect1 = detection_results[0]
+            detectX = (detect1.x2 + detect1.x1) / 2
+            detectY = (detect1.y2 + detect1.y1) / 2
+            print(detectX, detectY)
+            print(detect1.x1,detect1.x2,detect1.y1,detect1.y2)
+        except: 
+            print("list index out of range")
+        #try: 
+ 
+            #url = 'http://192.168.11.103/axis-cgi/com/ptz.cgi?rpan=5'
+            #response = requests.get(url)
+        #except: 
+            #print("request gescheitert")
+        #pass
