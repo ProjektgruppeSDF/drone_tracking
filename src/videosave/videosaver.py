75b67c6ca36294d15db0c5d4acd768493ac6c224
Projektgruppen User <s6parauh@uni-bonn.de>
2023-08-25 15:45:06 +0200

    Überarbeitung der Kamerasteuerung

--- src/videosave/videosaver.py
+++ src/videosave/videosaver.py
@@ -4,8 +4,8 @@ class Videosaver:
 
     def __init__(self) -> None:
         self.video = cv2.VideoWriter('Versuch1.avi', 
-                         cv2.VideoWriter_fourcc(*'MJPG'),
-                         10, (800,450))
+                         cv2.VideoWriter_fourcc('I','4','2','0'),
+                         15, (1920,1080))
         
     def write(self, image):
         self.video.write(image) 
