dcf93082175b025330aa78ae253af95525506992
Pablo <s6parauh@uni-bonn.de>
2023-08-31 13:00:38 +0200

    main refactored

--- src/main.py
+++ src/main.py
@@ -2,13 +2,27 @@ import os
 from config.global_config import disable_proxy_server_for_camera
 from detection.detection import Detector
 from detection.detection_model_factory import get_yolo_model_person, get_yolo_model_drone_simple, get_rtdetr_model_person, get_rtdetr_model_drone_simple, get_yolo_model_drone_good
-from detection.display_detection import display_image_with_detection, displayImage, displayTime
+from detection.display_detection import display_image_with_detection, displayImage
 from tracking.tracker import Tracker
 from camera.camera import Camera
 from videosave.videosaver import Videosaver
 from tracking.target_loss_monitor import TargetLossMonitorer
 from shutdown.shutdown import set_exit_strategy
 
+
+
+def process_image(camera, detector, tracker, videosaver, target_loss_monitorer):
+        image, time = camera.capture_image()
+        ptz = camera.get_camera_ptz_orientation()
+        detection_results = detector.detect(image)
+        if detection_results.exists:
+            display_image_with_detection(image, detection_results, target_loss_monitorer)
+            tracking_result = tracker.track(detection_results, time)
+            target_loss_monitorer.target_detection(time)
+            camera.move_camera(tracking_result, ptz["zoom"])
+
+        videosaver.write(image)
+        return detection_results.exists, image, time, ptz  
 
 
 
@@ -25,49 +39,29 @@ if __name__ == "__main__":
     tracker = Tracker()
     target_loss_monitorer = TargetLossMonitorer()
     videosaver = Videosaver()
-    
-    
 
 
     while True:
-
-        #Scan Modus
-        print("Starte Scan Modus")
+        # Scan Modus
+        print("Scan Modus")
         while True:
-            image,time = camera.capture_image()
-            ptz = camera.get_camera_ptz_orientation()
-            detection_results = detector.detect(image)
-            if(not detection_results.exists):
+            detection_exists, image, time, ptz = process_image(camera, detector, tracker, videosaver, target_loss_monitorer)
+            if not detection_exists:
                 camera.scan(ptz["tilt"])
-                displayImage(image,"Scan Modus",target_loss_monitorer)
-                videosaver.write(image)
+                displayImage(image, "Scan Modus", target_loss_monitorer)
             else:
-                display_image_with_detection(image, detection_results,target_loss_monitorer)
-                tracking_result =  tracker.track(detection_results,time)
-                camera.move_camera(tracking_result,ptz["zoom"])
-                videosaver.write(image)
                 break
-                
-        
-        #Tracking Modus
-        print("Starte Tracking Modus")
+
+        # Tracking Modus
+        print("Tracking Modus")
         while True:
-            image,time = camera.capture_image()
-            ptz = camera.get_camera_ptz_orientation()
-            detection_results = detector.detect(image)
-            if(detection_results.exists):
-                display_image_with_detection(image, detection_results,target_loss_monitorer)
-                tracking_result =  tracker.track(detection_results,time)
-                target_loss_monitorer.target_detection(time)
-                camera.move_camera(tracking_result,ptz["zoom"])
-            else:  
-                displayImage(image,"Tracking Modus", target_loss_monitorer)
+            detection_exists, image, time, ptz = process_image(camera, detector, tracker, videosaver, target_loss_monitorer)
+            if not detection_exists:
+                displayImage(image, "Tracking Modus", target_loss_monitorer)
                 target_loss_monitorer.no_target_detection(time)
                 if target_loss_monitorer.is_target_lost:
                     tracker.reinitialise()
-                    break          
-                
-            videosaver.write(image)
+                    break
 
     
         
